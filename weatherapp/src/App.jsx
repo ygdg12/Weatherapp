64888a09afd7bbdbd8861e3c40f2ee5eef8f8b6e
ygdg12 <yaredgirmab1234@gmail.com>
2024-12-29 22:43:04 +0300

    Initial commit

---
+++ weatherapp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weather from './components/Weather.jsx'
+function App() {
+  return (
+    <>
+     <div className="app">
+      <Weather/>
+      </div> 
+    </>
+  )
+}
+
+export default App
